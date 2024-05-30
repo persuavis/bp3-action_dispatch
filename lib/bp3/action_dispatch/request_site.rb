8d1172f20fcc0545a3b0d8dc8819edfbfb4d08dd
Wim den Braven <wimdenbraven@persuavis.com>
2024-05-30 15:46:07 -0700

    Use .site_class

--- lib/bp3/action_dispatch/request_site.rb
+++ lib/bp3/action_dispatch/request_site.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-# RequestSite is included in ActionDispatch::Request
-# it depends on RequestHost
 module Bp3
   module ActionDispatch
     module RequestSite
       def request_site
-        @request_site ||= Sites::Site.find_host(normalized_host)
+        @request_site ||= Bp3::ActionDispatch.site_class.find_host(normalized_host)
       end
     end
   end
